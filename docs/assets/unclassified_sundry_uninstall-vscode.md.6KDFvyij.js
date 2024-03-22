51129979de32280323be733ab882894b44ff8bd6
zd <476182045@qq.com>
2024-03-22 16:06:36 +0800

    f

---
+++ docs/assets/unclassified_sundry_uninstall-vscode.md.6KDFvyij.js
@@ -0,0 +1,7 @@
+import{_ as s,c as a,o as n,a5 as e}from"./chunks/framework.DFluhY12.js";const f=JSON.parse('{"title":"","description":"","frontmatter":{},"headers":[],"relativePath":"unclassified/sundry/uninstall-vscode.md","filePath":"unclassified/sundry/uninstall-vscode.md"}'),p={name:"unclassified/sundry/uninstall-vscode.md"},t=e(`<div class="language- vp-adaptive-theme"><button title="Copy Code" class="copy"></button><span class="lang"></span><pre class="shiki shiki-themes github-light github-dark vp-code"><code><span class="line"><span>C:\\Users\\47618\\.vscode</span></span>
+<span class="line"><span></span></span>
+<span class="line"><span>win+R 输入 %userprofile%</span></span>
+<span class="line"><span></span></span>
+<span class="line"><span>删除当前路径下的 .vscode 文件夹</span></span>
+<span class="line"><span></span></span>
+<span class="line"><span> %appdata%</span></span></code></pre></div>`,1),c=[t];function i(l,o,d,r,_,u){return n(),a("div",null,c)}const v=s(p,[["render",i]]);export{f as __pageData,v as default};
