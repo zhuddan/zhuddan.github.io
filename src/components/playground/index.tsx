3804dd59c854e2bb1fc89c642cb9b87123c4418c
zhuddan <yobaci@163.com>
2024-12-17 15:16:44 +0800

    fix

--- src/components/playground/index.tsx
+++ src/components/playground/index.tsx
@@ -1,14 +1,23 @@
-import type { SandpackFiles } from '@codesandbox/sandpack-react'
+import type { SandpackFiles, SandpackSetup } from '@codesandbox/sandpack-react'
 import { loadSandpackClient } from '@codesandbox/sandpack-client'
-import { Sandpack, SandpackCodeEditor, SandpackConsole, SandpackLayout, SandpackPreview, SandpackProvider } from '@codesandbox/sandpack-react'
+import {
+  Sandpack,
+  SandpackCodeEditor,
+  SandpackConsole,
+  SandpackLayout,
+  SandpackPreview,
+  SandpackProvider,
+} from '@codesandbox/sandpack-react'
 import React, { useMemo } from 'react'
 
-function Playground({ children }: { children: React.ReactNode }) {
+function Playground({
+  children,
+}: {
+  children: React.ReactNode
+}) {
   const files = useMemo(() => {
     function getFiles(element: React.ReactNode) {
-      const files: SandpackFiles = {
-
-      }
+      const files: SandpackFiles = { }
       const elements = Array.isArray(element)
         ? element
         : [element]
@@ -17,16 +26,27 @@ function Playground({ children }: { children: React.ReactNode }) {
         const element = elements[index]
         const result = getCode(element)
         if (result) {
-          const isEntry = Object.keys(files).length === 0
+          const isEntry = result.entry
+          const readOnly = result.entry
           files[result.fileName] = {
             code: result.code,
             active: isEntry,
+            readOnly: !!readOnly,
           }
           if (isEntry) {
-            files['index.ts'] = {
-              code: `import './${result.fileName}'`,
-              hidden: true,
-              active: false,
+            if (result.language !== 'html') {
+              files['index.ts'] = {
+                code: `import './${result.fileName}'`,
+                hidden: true,
+                active: false,
+              }
+            }
+            else {
+              files['index.ts'] = {
+                code: ` `,
+                hidden: true,
+                active: false,
+              }
             }
           }
         }
@@ -44,10 +64,15 @@ function Playground({ children }: { children: React.ReactNode }) {
           const language = element.props.className.replace('language-', '') || 'typescript'
           const code = element.props.children as string
           const fileName = element.props.metastring.match(/title="([^"]+)"/)?.[1]
+          const entry = element.props.metastring.match(/entry="([^"]+)"/)?.[1]
+          const readOnly = element.props.metastring.match(/readOnly="([^"]+)"/)?.[1]
+
           return {
             language,
             code,
             fileName,
+            entry,
+            readOnly,
           }
         }
       }
@@ -64,8 +89,12 @@ function Playground({ children }: { children: React.ReactNode }) {
       template="vanilla-ts"
     >
       <SandpackLayout>
-        <SandpackCodeEditor showTabs />
-        <SandpackConsole standalone />
+        {/* <SandpackCodeEditor showTabs /> */}
+        <SandpackPreview
+          showOpenInCodeSandbox={false}
+          showNavigator={false}
+        />
+        {/* <SandpackConsole standalone /> */}
       </SandpackLayout>
     </SandpackProvider>
   )
