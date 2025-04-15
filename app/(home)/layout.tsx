458843aa970ae3ba553b192d67feb1a4e732f6e1
zd <yobaci@163.com>
2025-04-15 16:48:43 +0800

    fix

--- app/(home)/layout.tsx
+++ app/(home)/layout.tsx
@@ -1,7 +1,135 @@
 import type { ReactNode } from 'react'
 import { baseOptions } from '@/app/layout.config'
 import { HomeLayout } from 'fumadocs-ui/layouts/home'
+import Link from 'next/link'
+
+const links: {
+  title: string
+  items: {
+    label: string
+    to?: string
+    href?: string
+  }[]
+}[] = [
+  {
+    title: 'Zdecode',
+    items: [
+      {
+        label: '归档',
+        to: '/docs/css/color-opacity',
+      },
+      {
+        label: '博客',
+        to: '/blog',
+      },
+    ],
+  },
+  {
+    title: '参考链接',
+    items: [
+      {
+        label: 'github',
+        href: 'https://github.com/zhuddan',
+      },
+      {
+        label: 'npm',
+        href: 'https://www.npmjs.com/~zhuddan',
+      },
+      {
+        label: 'zhuddan.github.io',
+        href: 'https://zhuddan.github.io/',
+      },
+    ],
+  },
+  {
+    title: '更多链接',
+    items: [
+      {
+        label: 'react',
+        to: 'https://react.dev/',
+      },
+      {
+        label: 'nextjs',
+        to: 'https://nextjs.org/',
+      },
+      {
+        label: 'fumadocs',
+        href: 'https://fumadocs.vercel.app/',
+      },
+    ],
+  },
+]
+
+const copyright = `Copyright © ${new Date().getFullYear()} zhuddan, Inc. Built with Docusaurus.`
+
+function Footer() {
+  return (
+    <footer className="mt-auto border-t bg-fd-card py-12 text-fd-secondary-foreground">
+      <div className="container justify-start flex flex-col gap-4 sm:flex-row  items-star sm:justify-between">
+        <div>
+          <p className="mb-1 text-sm font-semibold">Fumadocs</p>
+          <p className="text-xs">
+            Built with ❤️ by
+            {' '}
+            <a
+              href="https://fuma-dev.vercel.app"
+              rel="noreferrer noopener"
+              target="_blank"
+              className="font-medium"
+            >
+              Fuma
+            </a>
+          </p>
+        </div>
+        {
+          links.map((e) => {
+            return (
+              <div key={e.title} className=" h-full">
+                <p className="mb-1 text-sm font-semibold">{e.title}</p>
+                {
+                  e.items.map((link) => {
+                    return (
+                      <p className="text-xs" key={link.label}>
+
+                        {
+                          link.to
+                            ? (
+                                <Link
+                                  href={link.to}
+                                  className="font-medium"
+                                >
+                                  {link.label}
+                                </Link>
+                              )
+                            : (
+                                <a
+                                  href={link.href}
+                                  target="_blank"
+                                  className="font-medium"
+                                >
+                                  {link.label}
+                                </a>
+                              )
+                        }
+                      </p>
+                    )
+                  })
+                }
+              </div>
+            )
+          })
+        }
+      </div>
+      <p className="text-center text-sm mt-4">{copyright}</p>
+    </footer>
+  )
+}
 
 export default function Layout({ children }: { children: ReactNode }) {
-  return <HomeLayout {...baseOptions}>{children}</HomeLayout>
+  return (
+    <HomeLayout {...baseOptions}>
+      {children}
+      <Footer />
+    </HomeLayout>
+  )
 }
--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,21 +1,46 @@
+import clsx from 'clsx'
 import Link from 'next/link'
+
+function Hero() {
+  return (
+    <div className="relative z-[2] flex flex-col border-x border bg-fd-card/80 px-6 py-12 max-md:text-center md:px-12 md:pt-16 [.uwu_&]:hidden max-lg:overflow-hidden">
+      <h1 className="mb-8 text-4xl font-medium md:hidden">ZDecode</h1>
+      <h1 className="mb-8 max-w-[600px] text-4xl font-medium max-md:hidden">
+        Zd`s Blog.
+      </h1>
+      <p className="mb-8 text-fd-muted-foreground md:max-w-[80%] md:text-xl">
+        Zd`s Blog is a self-indulgent, opinionated personal blog — built solely for my own amusement.
+        Powered by Next.js and the elegant Fumadocs, because why settle for less when you can have both beauty and brains?
+      </p>
+      <div className="inline-flex items-center gap-3 max-md:mx-auto">
+        <Link
+          href="/docs/css/color-opacity"
+          className={clsx(
+            'inline-flex items-center justify-center text-sm font-medium ring-offset-fd-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-fd-ring disabled:pointer-events-none disabled:opacity-50 bg-gradient-to-b from-fd-primary to-fd-primary/60 text-fd-primary-foreground shadow-fd-background/20 hover:bg-fd-primary/90 h-11 px-6 rounded-full',
+            // buttonVariants({ size: 'lg', className: 'rounded-full' }),
+          )}
+        >
+          归档
+        </Link>
+        <Link
+          href="/blog"
+          target="_blank"
+          rel="noreferrer noopener"
+          className={clsx(
+            'inline-flex items-center justify-center text-sm font-medium ring-offset-fd-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-fd-ring disabled:pointer-events-none disabled:opacity-50 border from-fd-primary/10 shadow-fd-primary/10 hover:bg-fd-accent/50 hover:text-fd-accent-foreground h-11 px-6 rounded-full bg-fd-background',
+          )}
+        >
+          博客
+        </Link>
+      </div>
+    </div>
+  )
+}
 
 export default function HomePage() {
   return (
-    <main className="flex flex-1 flex-col justify-center text-center">
-      <h1 className="mb-4 text-2xl font-bold">Hello World</h1>
-      <p className="text-fd-muted-foreground">
-        You can open
-        {' '}
-        <Link
-          href="/docs"
-          className="text-fd-foreground font-semibold underline"
-        >
-          /docs
-        </Link>
-        {' '}
-        and see the documentation.
-      </p>
+    <main className="container relative max-w-[1100px] px-2 py-4 ">
+      <Hero />
     </main>
   )
 }
