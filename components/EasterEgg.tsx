0ac56b88487c1ea15f83abd84aedb6f8ac19c084
zd <yobaci@163.com>
2025-04-16 12:06:17 +0800

    123

--- components/EasterEgg.tsx
+++ components/EasterEgg.tsx
@@ -1,7 +1,28 @@
+'use client'
+import { DotLottieReact } from '@lottiefiles/dotlottie-react'
+import dayjs from 'dayjs'
 import React from 'react'
 
+const JUNJUN_BIRTHDAY = '04-16'// '06-19'
 export default function EasterEgg() {
+  const x = dayjs().format('MM-DD')
+  if (JUNJUN_BIRTHDAY === x) {
+    return <Cake />
+  }
   return (
-    <div>EasterEgg</div>
+    <div>
+      {x}
+    </div>
+  )
+}
+
+function Cake() {
+  return (
+    <DotLottieReact
+      src="/Animation - 1744775417256.lottie"
+      loop
+      autoplay
+      width={500}
+    />
   )
 }
