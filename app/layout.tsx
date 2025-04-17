25c6ba44045f69c054e963ad0f1b8a52afc7e087
zd <yobaci@163.com>
2025-04-17 12:09:30 +0800

    fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,9 +18,9 @@ export const metadata = ({
 export default function Layout({ children }: { children: ReactNode }) {
   return (
     <html lang="en" className={inter.className} suppressHydrationWarning>
-      <header>
-        <link rel="icon" href="/logo.svg" />
-      </header>
+      <head>
+        <link rel="icon" href="/logo.jpg" />
+      </head>
       <body className="flex flex-col min-h-screen">
         <RootProvider
           search={{
