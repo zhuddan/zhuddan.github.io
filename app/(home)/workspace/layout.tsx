535ad48c6e3daa0ef687e1023e29b954b89d2143
zd <yobaci@163.com>
2025-04-21 13:54:01 +0800

    123

---
+++ app/(home)/workspace/layout.tsx
@@ -0,0 +1,12 @@
+import type { ReactNode } from 'react'
+import { DocsBody } from 'fumadocs-ui/page'
+
+export default function Layout({ children }: { children: ReactNode }) {
+  return (
+    <main className="container relative max-w-[1100px] px-2 py-4">
+      <DocsBody>
+        {children}
+      </DocsBody>
+    </main>
+  )
+}
