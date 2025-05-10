c842f61764aaa692eca60282d9648ad90604274f
zd <yobaci@163.com>
2025-05-10 16:37:13 +0800

    fix

--- app/(home)/resume/layout.tsx
+++ app/(home)/resume/layout.tsx
@@ -1,9 +1,7 @@
 import type { ReactNode } from 'react'
 import { DocsBody } from 'fumadocs-ui/page'
-// import { usePathname } from 'next/navigation'
 
 export default function Layout({ children }: { children: ReactNode }) {
-  // const pathName = usePathname()
   return (
     <main className="container relative max-w-[1100px] px-2 py-4">
       <DocsBody>
